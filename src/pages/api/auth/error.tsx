17837d608e5ee7c26d89c9c9a06503f64c27e259
rulasssdeep <146976056+rulosodeep@users.noreply.github.com>
2024-12-01 17:50:19 -0600

    Update error.tsx

--- src/pages/api/auth/error.tsx
+++ src/pages/api/auth/error.tsx
@@ -3,7 +3,7 @@ export default function ErrorPage() {
       <div style={{ textAlign: "center", padding: "2rem" }}>
         <h1>Error de Autenticación</h1>
         <p>Ocurrió un error al iniciar sesión. Por favor, inténtalo de nuevo.</p>
-        <a href="/">Volver al inicio</a>
+        <link href="/">Volver al inicio</link>
       </div>
     );
   }
